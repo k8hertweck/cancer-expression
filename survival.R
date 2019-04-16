8754b4499c98620e913003ec04dc0b5b08b9ead5
k8hertweck <k8hertweck@gmail.com>
2019-04-16 10:39:18 -0700

    combinging code from kaplan-meier and survival

--- survival.R
+++ survival.R
@@ -1,20 +1,30 @@
-# survival plots
+# example kaplan-meier code
+# https://genomicsclass.github.io/book/pages/tcga.html
 
-install.packages('survminer')
-source("https://bioconductor.org/biocLite.R")
-biocLite("RTCGA.clinical")
-
-library(survminer)
-library(RTCGA.clinical)
 library(survival)
+library(survminer)
+library(TCGAbiolinks)
+library(dplyr)
 
-# load data
-survivalTCGA(BRCA.clinical,
-             extract.cols = "admin.disease_code") -> BRCA.survInfo
+# example clinical data
+clin_all <- GDCquery_clinic(project = "TCGA-BRCA", type = "clinical")
+# remove missing data for days to last followup
+clin <- clin_all %>%
+  filter(!is.na(days_to_last_follow_up))
+
+# survival plot
+ev <- 1*(clin$vital_status == 1)
+fut <- as.numeric(clin$days_to_last_follow_up)
+su <- Surv(fut, ev)
+plot(survfit(su~t_stage, data=clin), lwd=2, lty=1:4, xlim=c(0,2000))
+ntab <- table(clin$t_stage)
+ns <- paste("[n=", ntab, "]", sep="")
+legend(100, .4, lty=1:4, lwd=2, legend=paste(levels(clin$), ns))
 
 # fit model
 fit <- survfit(Surv(times, patient.vital_status) ~ admin.disease_code,
                data = BRCA.survInfo)
-
+?survfit
 # Visualize with survminer
 ggsurvplot(fit, data = BRCA.survInfo, risk.table = TRUE)
+?ggsurvplot
